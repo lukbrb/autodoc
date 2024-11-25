cd79b3480c2de6067a379df289cd0754eab7544e
Lucas Barbier <lucas.barbier@hpdia.com>
2024-11-25 16:03:44 +0100

    nouvelle api

--- autodoc/__init__.py
+++ autodoc/__init__.py
@@ -1,1 +1,13 @@
-from .autodoc import autodoc
+from functools import wraps
+from .core import Autodoc, check_and_update_file
+
+def autodoc(func):
+    @wraps(func)  # If we want to chain decorators
+    def wrapper(*args, **kwargs):
+        resultat = func(*args, **kwargs)
+
+        fulldoc = Autodoc(func, args, kwargs, resultat)
+        check_and_update_file(filename=f"{fulldoc.filename}i", file_content=fulldoc.generate_imports(), function_name="IMPORTS")  # Note: pas besoin de l'arg function_name pour écrire les imports.
+        check_and_update_file(filename=f'{fulldoc.filename}i', file_content=fulldoc.generate_full_doc(), function_name=fulldoc.funcname)
+        return resultat
+    return wrapper
